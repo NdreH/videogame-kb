89db5961136bd2ce71ca696fbfa30a12559866af
NdreH <aflopes81@gmail.com>
2024-09-04 17:09:30 +0100

    Initial commit

---
+++ src/main/java/dev/andrelopes/videogamekb/videogame_kb/VideogameKnowledgeBaseApplication.java
@@ -0,0 +1,13 @@
+package dev.andrelopes.videogamekb.videogame_kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameKnowledgeBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameKnowledgeBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andrelopes/videogamekb/videogame_kb/VideogameKnowledgeBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrelopes.videogamekb.videogame_kb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogameKnowledgeBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
